0b5a0f32ff91e32aa527045f6d15f1d25ea25640
梁星国 <jyyjkt@qq.com>
2018-12-04 15:14:26 +0800

    提交1.1

--- CNLiveDemo_LXG_Github.podspec
+++ CNLiveDemo_LXG_Github.podspec
@@ -39,5 +39,6 @@ TODO: 测试
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'MJExtension'
 end
---
+++ CNLiveDemo_LXG_Github/Classes/CNLiveKit.framework/Headers/Test.h
@@ -0,0 +1,18 @@
+//
+//  Test.h
+//  CNLiveTestFramework
+//
+//  Created by 张旭 on 2018/11/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
++ (void)testLog;
+
+@end
+
+NS_ASSUME_NONNULL_END
